dd3821e66e5fe3c4b3c96deda3a579b06a53d1a2
ihourglass <inversionhourglass@outlook.com>
2024-09-22 16:55:58 +0800

    Add a new extensions class CustomAttributeExtensions.

---
+++ Extensions/CustomAttributeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Fody;
+using System.Linq;
+
+namespace Mono.Cecil
+{
+    public static class CustomAttributeExtensions
+    {
+        public static bool IsCompilerGenerated(this TypeReference typeRef)
+        {
+            return IsCompilerGenerated(typeRef.ToDefinition().CustomAttributes);
+        }
+
+        public static bool IsCompilerGenerated(this MethodReference methodRef)
+        {
+            return IsCompilerGenerated(methodRef.ToDefinition().CustomAttributes);
+        }
+
+        private static bool IsCompilerGenerated(Collections.Generic.Collection<CustomAttribute> customAttributes)
+        {
+            return customAttributes.Any(x => x.AttributeType.Is(Constants.TYPE_CompilerGeneratedAttribute) || x.AttributeType.Is(Constants.TYPE_Runtime_CompilerGeneratedAttribute));
+        }
+    }
+}
